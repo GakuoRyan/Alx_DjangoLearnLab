8bffb1a547cae4dcc50abb54079a304b4d7b3325
GakuoRyan <rwgakuo@gmail.com>
2024-10-15 21:11:34 +0300

    Add files via upload

---
+++ advanced_features_and_security/bookshelf/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(default=2000)
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
+
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view post"),
+            ("can_create", "Can create post"),
+            ("can_edit", "Can edit post"),
+            ("can_delete", "Can delete post"),
+        ]
